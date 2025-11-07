5055a8158535a57434accdc6ff522e50408a1678
SamuelPerezC <samuelperezcontreras@gmail.com>
2025-11-06 20:52:13 -0500

    Update principal.py

--- CARROS/principal.py
+++ CARROS/principal.py
@@ -1,11 +1,14 @@
 from Vehiculos_padre import Vehiculos
 from deportivo_hijo import Deportivo_hijo
 
-# +++++++vcodigo principal ++++++++
+#================ codigo principal =========================
 obj_vehiculo_padre = Vehiculos()
-obj_vehiculo_padre.arranque(modelo, color, motor,numero_puertas, capacidad_pasajeros, tipo_combustible, dato_techo)
+obj_vehiculo_padre.arranque()
 
 obj_hijo = Deportivo_hijo()
 obj_hijo.arranque()
+
+
+
 
 
