1fa758b2a96561a2e986e336fcb5f84b5e1e9056
SamuelPerezC <samuelperezcontreras@gmail.com>
2025-11-11 16:33:22 -0500

    Add docstrings to Animales class methods
    Updated methods with docstrings for better documentation.

--- EJERCICIO ANIMALES/Animales.py
+++ EJERCICIO ANIMALES/Animales.py
@@ -1,36 +1,50 @@
 class Animales:
     def __init__(self, nombre, edad, habitad, dieta, tamaño, color):
-        self.nombre = nombre
-        self.edad = edad
-        self.habitad = habitad
-        self.dieta = dieta
-        self.tamaño = tamaño
-        self.color = color
+        """
+        Constructor de la clase base Animales
+        Inicializa los atributos fundamentales de cualquier animal
+        """
+        self.nombre = nombre      # Nombre identificador del animal
+        self.edad = edad          # Edad en años/meses según corresponda
+        self.habitad = habitad    # Entorno natural donde vive
+        self.dieta = dieta        # Tipo de alimentación (herbívoro, carnívoro, etc.)
+        self.tamaño = tamaño      # Dimensiones físicas del animal
+        self.color = color        # Coloración principal
 
-    # métodos
+    # MÉTODOS DE COMPORTAMIENTO ANIMAL BÁSICO
+
     def moverse(self):
-        print("Movimiento animal ejecutandose")
+        """Controla la locomoción y desplazamiento del animal"""
+        print("Movimiento animal ejecutándose")
 
-    def comunicacion():
-        print("Metodo de comunicacion animal ejecutandose")
+    def comunicacion(self):
+        """Gestiona los métodos de comunicación entre individuos"""
+        print("Método de comunicación animal ejecutándose")
 
-    def reproducción():
-        print("Tipo de apareamiento ejecutandose")
+    def reproduccion(self):
+        """Administra los procesos reproductivos de la especie"""
+        print("Tipo de apareamiento ejecutándose")
 
-    def adaptacion():
-        print("Adaptacion al entorno ambiental activado")
+    def adaptacion(self):
+        """Maneja la adaptación al entorno ambiental"""
+        print("Adaptación al entorno ambiental activado")
 
-    def instintos():
-        print("Instinto animal activandose")
+    def instintos(self):
+        """Controla los comportamientos instintivos naturales"""
+        print("Instinto animal activándose")
 
-    def descanso():
+    def descanso(self):
+        """Regula los períodos de descanso y reposo"""
         print("Tiempo de sueño o descanso animal activado")
 
-    def sueño():
+    def sueño(self):
+        """Gestiona los ciclos de sueño del animal"""
         print("Tiempo de sueño activado")
 
-    def interaccion_social():
-        print("Interaccion con su entorno activandose")
+    def interaccion_social(self):
+        """Administra las interacciones con otros animales"""
+        print("Interacción con su entorno activándose")
 
     def mostrar_atributos(self):
-        print(f"Nombre: {self.nombre} - Edad: {self.edad} - Habitad: {self.habitad} - Dieta: {self.dieta} - Tamaño: {self.tamaño} - Color: {self.color}")
+        """Muestra todos los atributos del animal en formato legible"""
+        print(f"Nombre: {self.nombre} - Edad: {self.edad} - Hábitat: {self.habitad} - Dieta: {self.dieta} - Tamaño: {self.tamaño} - Color: {self.color}")
